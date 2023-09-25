fc8c6475ade7183b56a598837f22861c87c1899a
Bestbest2546 <bst12546@gmail.com>
2023-09-25 17:09:31 +0700

    update finish

--- src/App.js
+++ src/App.js
@@ -7,31 +7,40 @@ import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import { IconButton } from "@mui/material";
-
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 import axios from "axios";
 
 function BasicExample() {
+  const [api, setApi] = React.useState("httpss://apimitphol.thetigerteamacademy.net/predict/");
+
+  const handleChange = (event) => {
+    setApi(event.target.value);
+  };
 
   // const URL = process.env.URL;
 
   const sendData = async () => {
     try {
       const response = await axios.post(
-        "https://apimitphol.thetigerteamacademy.net/predict/",
+        // "httpss://apimitphol.thetigerteamacademy.net/predict/",
+        api,
         {
-          "BrixF": Number(brixF),
-          "Fiber": Number(fiber)
+          BrixF: Number(brixF),
+          Fiber: Number(fiber),
         }
       );
-  
+
       console.log(response.data);
       setResult(response.data);
     } catch (error) {
       console.error("There was an error sending the data:", error);
     }
   };
-  
 
   // const [brix, setBrix] = React.useState("");
   // const [pol, setPol] = React.useState("");
@@ -54,7 +63,7 @@ function BasicExample() {
       <div className="nav">
         <img src={logo} alt="logo" />
       </div>
-      <div className="valueconatin">
+      <div className="valuecontain">
         <div className="addvalue">
           <TextField
             value={fiber}
@@ -88,19 +97,58 @@ function BasicExample() {
               </div>
             </text>
           </div>
-          <div className="button">
-            <Button
-              variant="primary"
-              onClick={sendData}
-              disabled={[fiber, brixF].some(
-                (value) => !value
-              )}
-            >
-              PREDICT
-            </Button>
-            <Button variant="secondary" onClick={clearValues}>
-              CLEAR
-            </Button>
+          <div className="containbutton">
+            <FormControl sx={{ m: 1, minWidth: 167 }}>
+              <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                onChange={handleChange}
+              >
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predict/">
+                Finalized_model
+                </MenuItem>
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predictM1/">
+                  Model 1
+                </MenuItem>
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predictM2/">
+                  Model 2
+                </MenuItem>
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predictM3/">
+                  Model 3
+                </MenuItem>
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predictM4/">
+                  Model 4
+                </MenuItem>
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predictM5/">
+                  Model 5
+                </MenuItem>
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predictM6/">
+                  Model 6
+                </MenuItem>
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predictM7/">
+                  Model 7
+                </MenuItem>
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predictM8/">
+                  Model 8
+                </MenuItem>
+                <MenuItem value="https://ccs.api.thetigerteamacademy.net/predictM9/">
+                  Model 9
+                </MenuItem>
+              </Select>
+              {/* <FormHelperText>With label + helper text</FormHelperText> */}
+            </FormControl>
+            <div className="button">
+              <Button
+                variant="primary"
+                onClick={sendData}
+                disabled={[fiber, brixF].some((value) => !value)}
+              >
+                PREDICT
+              </Button>
+              <Button variant="secondary" onClick={clearValues}>
+                CLEAR
+              </Button>
+            </div>
           </div>
         </div>
       </div>
